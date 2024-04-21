ae45ed6967327674d71a370b9abc415f5e36d56e
Richard Wen <rwen1@umd.edu>
2024-04-20 23:18:38 -0400

    fixed type 0 queries

--- algorithms/sigmod/big_index.h
+++ algorithms/sigmod/big_index.h
@@ -13,13 +13,15 @@
 #include <iostream>
 
 // limit, degree, alpha
-BuildParams default_build_params = BuildParams(200, 32, 1.175);
+//BuildParams default_build_params = BuildParams(200, 32, 1.175);
+BuildParams default_build_params = BuildParams(500, 64, 1.175);
 
 // k, beam size, cut, limit, degree limit
-QueryParams default_query_params = QueryParams(100, 150, 0.9, 1000, 100);
+//QueryParams default_query_params = QueryParams(100, 500, 0.9, 1000, 100);
+QueryParams default_query_params = QueryParams(100, 500, 1.35, 10000000, 100);
 
 
-const int exhaustive_fallback_cutoff = 1000;
+const float exhaustive_fallback_cutoff = 0.375;
 
 template<typename T, typename Point>
 struct VamanaIndex : public VirtualIndex<T, Point> {
@@ -55,22 +57,24 @@ struct VamanaIndex : public VirtualIndex<T, Point> {
         auto frontier = pairElts.first;
 
         for (size_t i = 0; i < k; i++) {
-            out[i] = naive_index.pr.real_index(frontier[i].second);
+            out[i] = naive_index.pr.real_index(frontier[i].first);
         }
     }
 
     void range_knn(Point& query, index_type* out, std::pair<float, float> endpoints, size_t k) override {
         // we assume that the distribution of timestamps is uniform, and the distance between the endpoints is some good approximation of the selectivity
-        float expected_matches = naive_index.pr.size() * (endpoints.second - endpoints.first);
+        float time_range = endpoints.second - endpoints.first;
 
-        if (expected_matches < exhaustive_fallback_cutoff) {
+        if (time_range < exhaustive_fallback_cutoff) {
             naive_index.range_knn(query, out, endpoints, k);
             return;
         } else { // otherwise we use overretrieval
             QueryParams qp = default_query_params;
-            qp.k = static_cast<int>((qp.k / (endpoints.second - endpoints.first)) * 1.44);
+            qp.k = qp.beamSize;
+            qp.limit = static_cast<int>(qp.limit / (endpoints.second - endpoints.first));
+            /*qp.k = static_cast<int>((qp.k / (endpoints.second - endpoints.first)) * 1.44);
             qp.beamSize = static_cast<int>((qp.beamSize / (endpoints.second - endpoints.first)) * 1.44);
-            qp.limit = static_cast<int>((qp.limit / (endpoints.second - endpoints.first)) * 1.44);
+            qp.limit = static_cast<int>((qp.limit / (endpoints.second - endpoints.first)) * 1.44);*/
  
             auto [pairElts, dist_cmps] = beam_search<Point, SubsetPointRange<T, Point, PointRange<T, Point>, uint32_t>, index_type>(query, G, naive_index.pr, 0, qp);
 
@@ -78,20 +82,16 @@ struct VamanaIndex : public VirtualIndex<T, Point> {
 
             // filter to the points which are in the range
             size_t found = 0;
-            for (size_t i = 0; i < frontier.size(); i++) {
+            for (size_t i = 0; i < frontier.size() && found < k; i++) {
                 if (naive_index.timestamps[frontier[i].first] >= endpoints.first && naive_index.timestamps[frontier[i].first] <= endpoints.second) {
                     out[found] = naive_index.pr.real_index(frontier[i].first);
                     found++;
                 }
-                if (found == k) {
-                    break;
-                }
             }
 
             if (found < k) {
-                // std::cout << "Warning: not enough points in range" << std::endl;
+                std::cout << "Warning: not enough points in range" << std::endl;
             }
-
         }
     }
 };
