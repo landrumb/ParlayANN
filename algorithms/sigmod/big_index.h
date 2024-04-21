31092d8aae4064faa7adde6d1a7cee72b9b37503
Ben Landrum <8enlandrum@gmail.com>
2024-04-20 20:14:21 -0400

    small errors

--- algorithms/sigmod/big_index.h
+++ algorithms/sigmod/big_index.h
@@ -13,10 +13,10 @@
 #include <iostream>
 
 // limit, degree, alpha
-const BuildParams default_build_params = BuildParams(200, 32, 1.175);
+BuildParams default_build_params = BuildParams(200, 32, 1.175);
 
 // k, beam size, cut, limit, degree limit
-const QueryParams default_query_params = QueryParams(100, 150, 0.9, 1000, 100);
+QueryParams default_query_params = QueryParams(100, 150, 0.9, 1000, 100);
 
 template<typename T, typename Point>
 struct VamanaIndex : public VirtualIndex<T, Point> {
