c92ebef126e962023fca603df6951973b88493d9
Ben Landrum <8enlandrum@gmail.com>
2024-04-20 22:19:42 -0400

    overretrieval

--- algorithms/sigmod/big_index.h
+++ algorithms/sigmod/big_index.h
@@ -18,6 +18,9 @@ BuildParams default_build_params = BuildParams(200, 32, 1.175);
 // k, beam size, cut, limit, degree limit
 QueryParams default_query_params = QueryParams(100, 150, 0.9, 1000, 100);
 
+
+const int exhaustive_fallback_cutoff = 500;
+
 template<typename T, typename Point>
 struct VamanaIndex : public VirtualIndex<T, Point> {
     NaiveIndex<T, Point> naive_index;
@@ -57,6 +60,36 @@ struct VamanaIndex : public VirtualIndex<T, Point> {
     }
 
     void range_knn(Point& query, index_type* out, std::pair<float, float> endpoints, size_t k) override {
-        naive_index.range_knn(query, out, endpoints, k);
+        // we assume that the distribution of timestamps is uniform, and the distance between the endpoints is some good approximation of the selectivity
+        float expected_matches = naive_index.pr.size() * (endpoints.second - endpoints.first);
+
+        if (expected_matches < exhaustive_fallback_cutoff) {
+            naive_index.range_knn(query, out, endpoints, k);
+            return;
+        } else { // otherwise we use overretrieval
+            QueryParams qp = default_query_params;
+            qp.k = static_cast<int>(qp.k * (endpoints.second - endpoints.first) * 1.44);
+
+            auto [pairElts, dist_cmps] = beam_search<Point, SubsetPointRange<T, Point, PointRange<T, Point>, uint32_t>, index_type>(query, G, naive_index.pr, 0, qp);
+
+            auto frontier = pairElts.first;
+
+            // filter to the points which are in the range
+            size_t found = 0;
+            for (size_t i = 0; i < frontier.size(); i++) {
+                if (naive_index.timestamps[frontier[i].first] >= endpoints.first && naive_index.timestamps[frontier[i].first] <= endpoints.second) {
+                    out[found] = naive_index.pr.real_index(frontier[i].first);
+                    found++;
+                }
+                if (found == k) {
+                    break;
+                }
+            }
+
+            if (found < k) {
+                std::cout << "Warning: not enough points in range" << std::endl;
+            }
+
+        }
     }
 };
