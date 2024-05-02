5f0c0bf3884c51d918f2579e0736bf9c62635819
Ben Landrum <8enlandrum@gmail.com>
2024-05-02 12:01:11 -0400

    exeutables were running in the wrong order

--- algorithms/sigmod/run_and_record_csv.sh
+++ algorithms/sigmod/run_and_record_csv.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # build executables
-make query
-make measure_recall
+make query measure_recall
 
 # run executables
-./measure_recall -r data/contest-queries-release-1m-output-graph.bin \
-           data/gt-1m-1m.bin data/contest-queries-release-1m.bin
-
 ./query -r data/contest-data-release-1m.bin data/contest-queries-release-1m.bin \
                     data/contest-queries-release-1m-output-graph.bin
+
+./measure_recall -r data/contest-queries-release-1m-output-graph.bin \
+           data/gt-1m-1m.bin data/contest-queries-release-1m.bin
