550076ed487caea1bea193d7e1e087dee0887fc8
Ben Landrum <8enlandrum@gmail.com>
2024-11-22 11:21:29 -0800

    updated benchmark script

--- data_tools/groundtruth_bench.sh
+++ data_tools/groundtruth_bench.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
 #SBATCH --job-name=groundtruth_bench
 #SBATCH --output=groundtruth_bench_%j.out
 #SBATCH --error=groundtruth_bench_%j.err
 #SBATCH --time=00:30:00
-#SBATCH --partition=debug
+#SBATCH --qos=debug
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --account=m4776
+#SBATCH -C cpu
 
 cd /global/homes/l/landrum/ParlayANN/data_tools
 
@@ -16,10 +16,17 @@ make compute_groundtruth
 echo "Available cores: $(nproc --all)"
 echo "Running on $(hostname)"
 
-start_time=$(date +%s)
 echo "starting at $(date)"
 
+DATA_DIR="/global/homes/l/landrum/data/sift"
+slice_sizes=( 1000 5000 10000 50000 100000 500000 1000000 )
 
-time ./compute_groundtruth -base_path ~/data/sift/sift-1M \
-    -query_path ~/data/sift/query-10K -data_type uint8 \
-    -dist_func Euclidian -k 100 -gt_path ~/data/sift/GT/sift-1M.gt
+for slice_size in ${slice_sizes[@]}; do
+    echo "Computing groundtruth for slice of size $slice_size"
+    start_time=$(date +%s.%N)
+    ./compute_groundtruth -base_path ${DATA_DIR}/sift-128-euclidean_${slice_size}.fbin \
+        -query_path ${DATA_DIR}/sift-128-euclidean_queries_10000.fbin -data_type float \
+        -dist_func Euclidian -k 100 -gt_path ${DATA_DIR}/GT/test.gt
+    end_time=$(date +%s.%N)
+    echo $start_time $end_time $slice_size >> groundtruth_benchmarks.txt
+done
