e141b56fc0a23d8ad0b2ccc1ada8790d0adf76e9
Ben Landrum <8enlandrum@gmail.com>
2024-12-03 13:07:42 -0800

    new plots

--- data_tools/plots.py
+++ data_tools/plots.py
@@ -6,6 +6,10 @@ import matplotlib.pyplot as plt
 strong_scaling = pd.read_csv('groundtruth_strong_scaling.txt', sep=' ')
 strong_scaling['time'] = strong_scaling['end'] - strong_scaling['start']
 strong_scaling.sort_values('nproc', inplace=True)
+
+# only using gist values
+strong_scaling = strong_scaling[strong_scaling['dataset'] == 'gist']
+
 strong_scaling
 
 # %%
@@ -30,6 +34,31 @@ ax[1].set_ylabel('Time (s)')
 plt.show()
 
 # %%
+# load scaling data
+scaling = pd.read_csv('groundtruth_scaling.txt', sep=' ')
+scaling['time'] = scaling['end'] - scaling['start']
 
 
+scaling
+# %%
+scaling['bpps'] = scaling['data_size'] / scaling['time']
 
+# %%
+original = scaling[scaling['q_block'] == 1]
+blocked = scaling[scaling['q_block'] == 100]
+
+fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+ax[0].plot(original['data_size'], original['bpps'], label='Original')
+ax[0].plot(blocked['data_size'], blocked['bpps'], label='Blocked')
+ax[0].set_xlabel('Base points')
+ax[0].set_ylabel('Base points per second')
+ax[0].legend()
+
+ax[1].plot(original['data_size'], original['time'], label='Original')
+ax[1].plot(blocked['data_size'], blocked['time'], label='Blocked')
+ax[1].set_xlabel('Base points')
+ax[1].set_ylabel('Time (s)')
+
+plt.show()
+
+# %%
