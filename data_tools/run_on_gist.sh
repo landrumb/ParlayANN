f8aa8c7fc2bdacecf68f6044d46cb67977e1b650
anmolkabra <ak2426@cornell.edu>
2024-12-18 04:51:17 -0800

    Update script to run on default gist if no args specified

--- data_tools/run_on_gist.sh
+++ data_tools/run_on_gist.sh
@@ -1,35 +1,46 @@
 #!/bin/bash
 
-# check for input argument
-if [ "$#" -ne 1 ]; then
-    echo "usage: $0 <slice_size>"
-    exit 1
-fi
-
-# input argument
-slice_size=$1
-
 # paths and configurations
-DATA_DIR="/pscratch/sd/l/landrum/data/gist/slices"
-QUERY_PATH="${DATA_DIR}/query_10000.fbin"
-GT_PATH="${DATA_DIR}/GT/test.gt"
-EXECUTABLE="./compute_groundtruth"
+EXECUTABLE="compute_groundtruth"
+make $EXECUTABLE
+
+# check for input argument
+if [ "$#" -gt 1 ]; then
+    echo "Usage: $0"
+    echo "      $0 <slice_size>"
+    exit 1
+elif [ "$#" -eq 0 ]; then
+    # No input argument, run on full GIST dataset
+    DATA_DIR="/pscratch/sd/l/landrum/data/gist"
+    BASE_PATH="${DATA_DIR}/gist_learn.fbin"
+    QUERY_PATH="${DATA_DIR}/gist_query.fbin"
+    GT_PATH="${DATA_DIR}/test.gt"
+    echo "Running groundtruth computation for full GIST dataset..."
+else
+    # Input argument defines slice size, run on GIST slice
+    DATA_DIR="/pscratch/sd/l/landrum/data/gist/slices"
+    slice_size=$1
+    BASE_PATH="${DATA_DIR}/base_${slice_size}.fbin"
+    QUERY_PATH="${DATA_DIR}/query_10000.fbin"
+    GT_PATH="${DATA_DIR}/GT/test.gt"
+    # format the slice size with commas
+    formatted_size=$(printf "%'d" "$slice_size")
+    # run the executable
+    echo "Running groundtruth computation for slice size ${formatted_size}..."
+fi
+echo "- BASE_PATH: ${BASE_PATH}"
+echo "- QUERY_PATH: ${QUERY_PATH}"
+echo "- GT_PATH: ${GT_PATH}"
 
 # check if the base file exists
-BASE_PATH="${DATA_DIR}/base_${slice_size}.fbin"
 if [ ! -f "$BASE_PATH" ]; then
     echo "error: base file $BASE_PATH does not exist."
     exit 1
 fi
 
-# format the slice size with commas
-formatted_size=$(printf "%'d" "$slice_size")
-
-# run the executable
-echo "Running groundtruth computation for slice size ${formatted_size}"
 start_time=$(date +%s.%N)
 
-$EXECUTABLE \
+"./$EXECUTABLE" \
     -base_path "${BASE_PATH}" \
     -query_path "${QUERY_PATH}" \
     -data_type float \
